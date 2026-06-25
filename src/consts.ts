2f1df1dca40757f0b9aae2df8ff3170ba8314ccf
hanjoonbae <hanjoonbae.jobis.co>
2026-06-25 15:09:04 +0900

    fix

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = 'Hanjoon Bae';
-export const SITE_DESCRIPTION = 'AWS, Databricks, 구현, 공부, 논문 리뷰를 차분히 쌓는 기술 블로그.';
+export const SITE_TITLE = 'HANJOON BAE';
+export const SITE_DESCRIPTION = 'materials science   ㅤ·ㅤ   data science   ㅤ·ㅤ   minimalism';
 export const SITE_URL = 'https://hanjoonbae.github.io';
